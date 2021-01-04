87a4b09cf75abab5cd3cbb284df967ec9092c30d
Jeren <jerenshamuhamedova@gmail.com>
2021-01-03 22:44:29 -0600

    Added Dairy to Jeren

--- src/Jeren/Dairy.java
+++ src/Jeren/Dairy.java
@@ -4,14 +4,42 @@ import Ayse.Product.Product;
 
 public class Dairy extends Product {
 
-    String dairyName;
-    String weight;
+    private String dairyName;
+    private double weight;
     public Dairy(String productID, int quantity, double productPrice, String dairyName) {
         super("Dairy", productID, quantity, productPrice);
         this.dairyName = dairyName;
     }
 
-    public Dairy(String productName, String productID, double productPrice) {
-        super(productName, productID, productPrice);
+    public Dairy( String productID, double productPrice, double weight, String dairyName) {
+        super("Dairy", productID, productPrice);
+        this.weight = weight;
+        this.dairyName = dairyName;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getDairyName() {
+        return dairyName;
+    }
+
+    @Override
+    public double calculateCost() {
+        if (getQuantity()>0 && getQuantity()<20)
+            return super.calculateCost();
+        else if (getWeight()>0)
+            return getWeight() * getProductPrice();
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Dairy{" +
+                "weight = " + weight +
+                ", dairyName = '" + dairyName + '\'' +
+                ", total price = $"+ calculateCost() +"}";
+
     }
 }
---
+++ src/Jeren/DairyObjects.java
@@ -0,0 +1,8 @@
+package Jeren;
+
+public class DairyObjects {
+    public static void main(String[] args) {
+        Dairy obj1 = new Dairy("D123",10.0, 0.90,"egg");
+        System.out.println(obj1);
+    }
+}
